afa56513baaed4cea619de16c7bcbf1af2aaa675
wframpton <wframpton@gmail.com>
2017-05-26 12:40:02 -0500

    Adding HelloWorld to github bullpen

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World...Testing for GitHub");
+        System.out.println("Hello World...Testing for GitHub2");
     }
     
 }
